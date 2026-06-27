08a4e1735d1105130f4909736b3ba56b0b8d02c5
JCPrime01 <vanzokiller@gmail.com>
2026-06-27 00:08:37 -0300

    chore: shareCode 4OMK9O7IAGY -> HTTPSDDSUGESTAOCC (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/vzyrqvrc?shareCode=4OMK9O7IAGY&afp=bd&home=1' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/vzyrqvrc?shareCode=HTTPSDDSUGESTAOCC&afp=bd&home=1' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
